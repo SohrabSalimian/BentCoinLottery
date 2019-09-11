d1cf9613fb4182c6a363657190aee56c1b0f69ef
Sohrab Salimian <salimian@uchicago.edu>
2019-09-11 14:21:59 -0400

    renaming variables
    renaming variables

--- lottery.R
+++ lottery.R
@@ -11,15 +11,16 @@ library(stats)
 
 # Small edit! from class 1!
 
-arr <- array(0,10)
-for (i in 1:10){
-  arr[i] <- if (runif(1) < 0.1) 1 else 0
-}
+#length_of_lottery_ticket <- array(0,10)
+#for (i in 1:10){
+#  length_of_lottery_ticket[i] <- if (runif(1) < 0.1) 1 else 0
+#}
 
 for (j in 1:100){
-  arr <- array(0,10)
+  length_of_lottery_ticket <- array(0,10)
   for (i in 1:10){
-    arr[i] <- if (runif(1) < 0.1) 1 else 0
+    length_of_lottery_ticket[i] <- if (runif(1) < 0.1) 1 else 0
   }
-  print(arr)
+  print(length_of_lottery_ticket)
 }
+
